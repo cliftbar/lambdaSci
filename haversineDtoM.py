5827dc8d489a695c01803f2d0e7e6f518acfe824
cliftbar <cwbarclift@gmail.com>
2018-02-23 05:47:20 +0000

    adding unit path

--- haversineDtoM.py
+++ haversineDtoM.py
@@ -1,5 +1,11 @@
 import json
 import math
+import enum
+
+class distanceUnitsFromMeters(enum.Enum):
+    meters = 1
+    kilometers = 0.001
+    miles = 0.000621371
 
 def haversineDegreesToMeters(lat_1, lon_1, lat_2, lon_2):
     """
@@ -21,21 +27,54 @@ def haversineDegreesToMeters(lat_1, lon_1, lat_2, lon_2):
          (math.sin(delta_lon / 2) ** 2))
     c = 2 * math.atan2(math.sqrt(a), math.sqrt(1 - a))
     return r * c
+    
+def haversine(lat1, lon1, lat2, lon2, unit = distanceUnitsFromMeters.meters.name):
+    d = haversineDegreesToMeters(lat1, lon1, lat2, lon2)
+    c = distanceUnitsFromMeters[unit].value
+    return d * c
 
 def handler(event, context):
-    # e = event["x"]
-    
-    lat1 = float(event['queryStringParameters']['lat1'])
-    lat2 = float(event['queryStringParameters']['lat2'])
-    lon1 = float(event['queryStringParameters']['lon1'])
-    lon2 = float(event['queryStringParameters']['lon2'])
-    
-    d = haversineDegreesToMeters(lat1, lon1, lat2, lon2)
-    
-    data = {
-        'distance': d
-        #,'event': format(event)
-    }
-    return {'statusCode': 200,
-            'body': json.dumps(data),
-            'headers': {'Content-Type': 'application/json'}}
+    ret = {}
+    try:
+        try:
+            lat1 = float(event['queryStringParameters']['lat1'])
+            lat2 = float(event['queryStringParameters']['lat2'])
+            lon1 = float(event['queryStringParameters']['lon1'])
+            lon2 = float(event['queryStringParameters']['lon2'])
+        except Exception as e:
+            raise Exception("Invalid query string: " + str(e))
+        
+        unit = None
+        try:
+            unit = event['pathParameters']['unit'] or 'meters'
+        except:
+            unit = 'meters'
+            
+        if unit not in distanceUnitsFromMeters.__members__:
+            raise Exception("Invalid path parameter: " + unit)
+        # if unit not in distanceUnitsFromMeters
+        #     raise Exception("unit not available")
+        d = haversine(lat1, lon1, lat2, lon2, unit)
+        #d = haversineDegreesToMeters(lat1, lon1, lat2, lon2)
+        
+        data = {
+            'distance': d
+            ,'unit': unit
+            #,'event': format(event)
+        }
+        ret = {'statusCode': 200,
+                'body': json.dumps(data),
+                'headers': {'Content-Type': 'application/json'}}
+    except KeyError as e:
+        ret = {'statusCode': 500
+                ,'body': 'invalid path or : ' + str(e)
+                ,'headers': {'Content-Type': 'application/json'}
+            }
+    except Exception as e:
+        ret = {'statusCode': 500
+                ,'body': str(e)
+                ,'headers': {'Content-Type': 'application/json'}
+            
+            }
+    return ret
+            
